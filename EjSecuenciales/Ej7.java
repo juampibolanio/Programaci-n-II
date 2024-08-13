f92ec1c26517563b87cbebeb2019163ae10245f7
Juan Pablo Bolanio <juampi360786@gmail.com>
2024-08-13 16:02:24 -0300

    se hizo el ejercicio 7

--- EjSecuenciales/Ej7.java
+++ EjSecuenciales/Ej7.java
@@ -2,5 +2,23 @@ package EjSecuenciales;
 /*
 Un profesor desea saber qué porcentaje de hombres y qué porcentaje de mujeres hay en un grupo de estudiantes.
  */
+import java.util.Scanner;
 public class Ej7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingrese el número de hombres: ");
+        double numHombres = sc.nextInt();
+        System.out.println("Ingrese el número de mujeres: ");
+        double numMujeres = sc.nextInt();
+
+        double total = numHombres + numMujeres;
+        
+        double porcentajeHombres = (numHombres / total) * 100;
+        double porcentajeMujeres = (numMujeres / total) * 100;
+
+        System.out.println("En el grupo de estudiantes el % " + porcentajeHombres + " son hombres y el % " + porcentajeMujeres + " son mujeres.");
+
+        sc.close();
+    }
 }
