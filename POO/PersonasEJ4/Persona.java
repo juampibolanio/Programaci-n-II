dd5351f40f736277ceb57b3d32a6d6cbe1581dbf
JuampiBolanio <juampi360786@gmail.com>
2024-08-25 12:10:27 -0300

    nuevo ejercicio de poo realizado

---
+++ POO/PersonasEJ4/Persona.java
@@ -0,0 +1,119 @@
+package POO.PersonasEJ4;
+
+import java.util.Random;
+
+public class Persona {
+    private String nombre;
+    private int edad;
+    private int dni;
+    private char sexo;
+    private double peso;
+    private double altura;
+
+    public Persona() {
+        this.dni = generaDNI(); 
+        this.sexo = 'H'; 
+    }
+
+    public Persona(String nombre, int edad, String sexo) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.dni = generaDNI(); 
+        comprobarSexo(sexo);
+    }
+
+    public Persona(String nombre, int edad, String sexo, double peso, double altura) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.dni = generaDNI(); 
+        comprobarSexo(sexo);
+        this.peso = peso;
+        this.altura = altura;
+    }
+
+    public String calcularIMC() {
+        double imc = this.peso / Math.pow(this.altura, 2);
+    
+        if (imc < 20) {
+            return "Se encuentra debajo de su peso ideal.";
+        } else if (imc >= 20 && imc <= 25) {
+            return "Se encuentra en su peso ideal.";
+        } else {
+            return "Se encuentra con sobrepeso.";
+        }
+    }
+
+    public boolean esMayorDeEdad() {
+        return this.edad >= 18;
+    }
+
+    public void comprobarSexo(String sexo) {
+        char sexoChar = sexo.charAt(0); // Corregido para tomar el primer carácter
+        if (sexoChar != 'H' && sexoChar != 'M') {
+            this.sexo = 'H'; // Valor por defecto si el sexo no es válido
+        } else {
+            this.sexo = sexoChar;
+        }
+    }
+
+    public int generaDNI() {
+        Random random = new Random();
+        return 10000000 + random.nextInt(90000000); 
+    }
+
+    @Override
+    public String toString() {
+        return "Persona |  Nombre: " + nombre + " - Edad :" + edad + " - DNI: " + dni + " - Sexo: " + sexo + " - Peso: " + peso
+                + " - Altura: " + altura;
+    }
+
+    // Métodos setters
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setSexo(String sexo) {
+        comprobarSexo(sexo);
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    // Métodos getters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+}
---
+++ POO/PersonasEJ4/RegistroCivil.java
@@ -0,0 +1,43 @@
+package POO.PersonasEJ4;
+
+import java.util.Scanner;
+
+public class RegistroCivil {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("---------] Bienvenido al Registro Civil [---------");
+        System.out.println("-| A continuación, ingrese los datos solicitados |-");
+        System.out.print("Ingrese el nombre y apellido: ");
+        String nombre = sc.nextLine();
+
+        System.out.print("Ingrese la edad: ");
+        int edad = sc.nextInt();
+
+        System.out.print("Ingrese el sexo (M/F): ");
+        String sexo = sc.next();
+
+        System.out.print("Ingrese la altura en metros: ");
+        double altura = sc.nextDouble();
+
+        System.out.print("Ingrese el peso en kilogramos: ");
+        double peso = sc.nextDouble();
+
+        Persona persona1 = new Persona(nombre, edad, sexo, peso, altura);
+
+        persona1.generaDNI(); // generar DNI
+        persona1.comprobarSexo(sexo);
+        String imc = persona1.calcularIMC();
+
+        System.out.println("----------] Nombre completo: " + persona1.getNombre() + " [----------");
+        System.out.println("- Edad: " + persona1.getEdad() + " años");
+        System.out.println("- Sexo: " + persona1.getSexo());
+        System.out.println("- Altura: " + altura + " m");
+        System.out.println("- Peso: " + peso + " kg");
+        System.out.println("- Índice de masa corporal: " + imc);
+        System.out.println("- ¿Es mayor de edad? " + persona1.esMayorDeEdad());
+        System.out.println("-------------------------------------------------------------------");
+
+        sc.close();
+    }
+}
